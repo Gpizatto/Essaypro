77404c3e9fc5cd1562b3ae51c515a09c63320dcb
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2026-04-13 17:21:11 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { BrandingProvider } from "@/contexts/BrandingContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider>
+  <BrandingProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrandingProvider>
   </ThemeProvider>,
 );
