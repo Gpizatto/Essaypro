7f15c9dd5ba5e0d163db0ac1cbd79bd9b982267a
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2026-04-24 10:27:32 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
-import { ThemeProvider } from "@/contexts/ThemeContext";
 import { BrandingProvider } from "@/contexts/BrandingContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider>
   <BrandingProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </BrandingProvider>
-  </ThemeProvider>,
 );
 
 // U-08: Registrar Service Worker para cache offline
